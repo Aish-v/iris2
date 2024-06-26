222d013f3d1d9947a9ccb955b92fe1cc897d98c9
Aishwarya <aish08003@gmail.com>
2024-06-26 13:07:59 +0530

    squa

--- v.py
+++ v.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 from matplotlib import pyplot as plt
-
+#jhegfbjhgbjkdfhv
 ## Data modeling
 import tensorflow as tf
 from keras import Sequential
